fdae1c0540f0a6b1a7eb4c2c27ffd51b37fc49d4
Juechen <wjc.zhw@gmail.com>
2017-12-08 12:44:03 +1300

    FixGCE Bug

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,29 +1,36 @@
 package com.udacity.gradle.builditbigger;
 
 
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
 import com.example.android.jokedisplay.JokeActivity;
 import com.example.android.jokesourcelibrary.JokeSource;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import java.io.IOException;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private JokeSource mJokeSource = new JokeSource();
-
     private String mJoke;
+
+    private Intent jokeIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
@@ -52,17 +59,61 @@ public class MainActivity extends AppCompatActivity {
     public void tellJoke(View view) {
 
         new JokeAsyncTask().execute();
-
-        Intent jokeIntent = new Intent(this, JokeActivity.class);
-
-        jokeIntent.putExtra(JokeActivity.jokeName, mJoke);
-
-        startActivity(jokeIntent);
     }
 
-    public void setJoke(String joke){
+    public void setJoke(String joke) {
 
         mJoke = joke;
+    }
+
+    public class JokeAsyncTask extends AsyncTask<Void, Void, String> {
+        private MyApi myApiService = null;
+        @SuppressLint("StaticFieldLeak")
+
+
+        private JokeSource mJokeSource = new JokeSource();
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+            if (myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            String name = mJokeSource.Joke();
+
+            try {
+                return name;
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            setJoke(result);
+
+            jokeIntent = new Intent(getBaseContext(), JokeActivity.class);
+
+            jokeIntent.putExtra(JokeActivity.jokeName, mJoke);
+
+            startActivity(jokeIntent);
+        }
     }
 
 }
